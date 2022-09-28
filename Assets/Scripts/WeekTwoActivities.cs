713246425e77d71144453cb54fbd0758ebcb85c1
JohnBundalian <114544102+JohnBundalian@users.noreply.github.com>
2022-09-28 12:01:27 +1000

    WeekTwoActivities
    GAD170

---
+++ Assets/Scripts/WeekTwoActivities.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JohnBundalian
+{
+    public class WeekTwoActivities : MonoBehaviour
+    {
+        private string favouriteGame = "World of Warcraft";
+        private float hoursPlayed = 3200f;
+        private float costOfGame = 50f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("My favourite game is " + favouriteGame + "!");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
